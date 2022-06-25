214f73f574c77a364e38c7fa8bb3229d562e46f7
stefl14 <stefl141193@gmail.com>
2022-06-25 10:36:10 +0100

    Add sql init script and update docker compose stack to run this script and load pdf download metadata.

---
+++ init.sql
@@ -0,0 +1,18 @@
+-- Creation of papers table
+DROP TABLE IF EXISTS public."papers" CASCADE;
+CREATE TABLE public."papers"
+(
+  name text,
+  scholar_link text,
+  doi text,
+  bibtext bool,
+  pdf_name text,
+  year int,
+  scholar_page text,
+  journal text,
+  downloaded bool,
+  downloaded_from text,
+  authors text
+);
+
+\copy public."papers" FROM '/var/lib/pg_data/results.csv' DELIMITER ',' CSV HEADER;
