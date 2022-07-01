57b6d5bc6bc4e5dc16b55194c20ae89bd0f46204
Stefan <stefl141193@gmail.com>
2022-07-01 16:53:47 +0100

    Update script.

--- scripts/fep_pypaperbot_example.sh
+++ scripts/fep_pypaperbot_example.sh
@@ -1,2 +1,6 @@
 # Get papers that relate to Karl Friston's Free Energy Principle from 2010 onwards and put them in the downloads directory.
+# Refresh.
+rm -rf downloads/*
 python -m PyPaperBot --query="Free Energy Principle" --scholar-pages=3  --min-year=2010 --dwn-dir="downloads" --scihub-mirror="https://sci-hub.yncjkj.com/"
+# Move results.csv to ../pg_data/results.csv.
+mv downloads/results.csv ../pg_data/results.csv
